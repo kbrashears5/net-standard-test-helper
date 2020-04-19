6062c79cad200047f8da4f0038cfb500067cb76d
Brashears <Kevin.Brashears@fhr.com>
2020-04-19 16:04:22 -0500

    initial commit

---
+++ c#/TestHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+using System.Reflection;
+
+namespace TestHelper
+{
+    public static class TestHelper
+    {
+
+        /// <summary>
+        /// While you can use Assert.Throws(), this method asserts that the correct parameter was thrown
+        /// </summary>
+        /// <param name="ex">Exception to check</param>
+        /// <param name="paramName">Paramater name to assert was null</parm>
+        public static void AssertArgumentNullException(ArgumentNullException ex,
+            string paramName)
+        {
+            var exceptionText = $"Value cannot be null.\r\nParameter name: {paramName}";
+
+            AssertExceptionText(ex,
+                exceptionText);
+        }
+
+        /// <summary>
+        /// Assert that the Exception text is what was expected
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="expectedExceptionText"></param>
+        public static void AssertExceptionText(Exception ex,
+            string expectedExceptionText)
+        {
+            Assert.Equal(expectedExceptionText,
+                ex.Message);
+        }
+    }
+}
---
+++ c#/TestValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestHelper
+{
+    public static class TestValues
+    {
+        public static string EmptyString { get; } = string.Empty;
+
+        public static object EmptyObject { get; } = new object();
+
+        public static object NullObject { get; } = null;
+
+        public static IEnumerable<string> NullStringList { get; }
+
+        public static IEnumerable<string> EmptyStringList { get; } = new List<string>();
+
+        public static EventArgs EventArgs { get; } = EventArgs.Empty;
+    }
+}
